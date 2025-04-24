4be99e35dfcde973d617e61fabbc89931f358a3a
Achraf <achraf.benaomar@gmail.com>
2025-04-24 13:27:15 +0100

    fix: update password

--- apps/frontend/src/lib/auth-client.ts
+++ apps/frontend/src/lib/auth-client.ts
@@ -1,18 +1,23 @@
-import { createAuthClient } from "better-auth/react";
-import {
-  emailOTPClient,
-  inferAdditionalFields,
-  usernameClient,
-} from "better-auth/client/plugins";
 import type { auth } from "@celluloid/auth";
-import { signupAsStudentClient } from "@celluloid/auth/plugins"
-
+import { signupAsStudentClient } from "@celluloid/auth/plugins";
+import {
+	emailOTPClient,
+	inferAdditionalFields,
+	usernameClient,
+} from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  plugins: [signupAsStudentClient(), usernameClient(), emailOTPClient(), inferAdditionalFields<typeof auth>()],
+	plugins: [
+		signupAsStudentClient(),
+		usernameClient(),
+		emailOTPClient(),
+		inferAdditionalFields<typeof auth>(),
+	],
 });
 
 export type Session = typeof authClient.$Infer.Session;
 export type User = typeof authClient.$Infer.Session.user;
 
-export const { signIn, signUp, signOut, useSession } = authClient;
+export const { signIn, signUp, signOut, useSession, changePassword } =
+	authClient;

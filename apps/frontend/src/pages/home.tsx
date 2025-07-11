eb49b27f648d2b3933252ccfff2cad8a61c5b5fb
Younes <younes.benaomar@gmail.com>
2025-07-11 18:38:46 +0100

    feat: add video tutoral

--- apps/frontend/src/pages/home.tsx
+++ apps/frontend/src/pages/home.tsx
@@ -1,11 +1,11 @@
 import {
-  Box,
-  CircularProgress,
-  Container,
-  Fade,
-  Link,
-  Paper,
-  Stack,
+	Box,
+	CircularProgress,
+	Container,
+	Fade,
+	Link,
+	Paper,
+	Stack,
 } from "@mui/material";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
@@ -19,234 +19,234 @@ import { useNavigate } from "react-router-dom";
 import { useSession } from "~/lib/auth-client";
 
 import { ProjectGrid } from "~components/home/ProjectGrid";
-import { LogoSign } from "~components/LogoSign";
 import { StudentsIcon } from "~components/StudentsIcon";
 import { TeacherIcon } from "~components/TeacherIcon";
 
 export const HomePage: React.FC = () => {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  const { t } = useTranslation();
-  const navigate = useNavigate();
+	const { t } = useTranslation();
+	const navigate = useNavigate();
 
-  const handleJoin = useCallback(() => {
-    if (!session) {
-      navigate("/signup-student", { state: { backgroundLocation: "/" } });
-    } else {
-      navigate("/join", { state: { backgroundLocation: "/" } });
-    }
-  }, [session, navigate]);
+	const handleJoin = useCallback(() => {
+		if (!session) {
+			navigate("/signup-student", { state: { backgroundLocation: "/" } });
+		} else {
+			navigate("/join", { state: { backgroundLocation: "/" } });
+		}
+	}, [session, navigate]);
 
-  const handleCreate = () => {
-    navigate("/create");
-  };
+	const handleCreate = () => {
+		navigate("/create");
+	};
 
-  return (
-    <Box>
-      <Box sx={{ backgroundColor: "brand.green" }}>
-        <Container maxWidth="lg">
-          <Grid container={true}>
-            <Grid item xs={12} sm={6}>
-              <Box
-                display={"flex"}
-                alignContent={"center"}
-                alignItems={"center"}
-                sx={{ height: "100%" }}
-              >
-                <Box display={"flex"} flexDirection={"column"}>
-                  {/* <Box sx={{ width: "40%" }}>
-                    <LogoSign />
-                  </Box> */}
-                  <Typography
-                    variant="h4"
-                    gutterBottom={true}
-                    fontFamily={"abril_fatfaceregular"}
-                  >
-                    {t("home.title")}
-                  </Typography>
+	return (
+		<Box>
+			<Box sx={{ backgroundColor: "brand.green" }}>
+				<Container maxWidth="lg">
+					<Grid container={true}>
+						<Grid item xs={12} sm={6}>
+							<Box
+								display={"flex"}
+								alignContent={"center"}
+								alignItems={"center"}
+								sx={{ height: "100%" }}
+							>
+								<Box display={"flex"} flexDirection={"column"}>
+									<Typography
+										variant="h4"
+										gutterBottom={true}
+										fontFamily={"abril_fatfaceregular"}
+									>
+										{t("home.title")}
+									</Typography>
 
-                  <Typography variant="subtitle1" gutterBottom={true}>
-                    {t("home.description")}
-                  </Typography>
-                  <Typography
-                    variant="h5"
-                    gutterBottom={true}
-                    fontFamily={"abril_fatfaceregular"}
-                  >
-                    {t("home.tutoriel.title")}
-                  </Typography>
-                  <Typography variant="subtitle1" gutterBottom={true}>
-                    <Trans i18nKey={"home.tutoriel.description"}>
-                      <Link
-                        href="https://joinpeertube.org/fr_FR"
-                        target="_blank"
-                        rel="noreferrer"
-                      />
-                    </Trans>
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    gutterBottom={true}
-                    fontFamily={"abril_fatfaceregular"}
-                  >
-                    <Trans i18nKey={"home.tutoriel.subtitle"} />
-                  </Typography>
+									<Typography variant="subtitle1" gutterBottom={true}>
+										{t("home.description")}
+									</Typography>
+									<Typography
+										variant="h5"
+										gutterBottom={true}
+										fontFamily={"abril_fatfaceregular"}
+									>
+										{t("home.tutoriel.title")}
+									</Typography>
+									<Typography variant="subtitle1" gutterBottom={true}>
+										<Trans i18nKey={"home.tutoriel.description"}>
+											<Link
+												href="https://joinpeertube.org/fr_FR"
+												target="_blank"
+												rel="noreferrer"
+											/>
+										</Trans>
+									</Typography>
+									<Typography
+										variant="h6"
+										gutterBottom={true}
+										fontFamily={"abril_fatfaceregular"}
+									>
+										<Trans i18nKey={"home.tutoriel.subtitle"} />
+									</Typography>
 
-                  <Typography variant="subtitle1" gutterBottom={true}>
-                    <Trans i18nKey={"home.tutoriel.link"} />
-                    <Link
-                      href={t("home.tutotial.link_url")}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {t("home.tutoriel.link_url")}
-                    </Link>
-                  </Typography>
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Stack
-                spacing={4}
-                paddingY={5}
-                marginX={10}
-                alignItems={"center"}
-              >
-                <Paper
-                  sx={{
-                    p: "2px 4px",
-                    display: "flex",
-                    flexDirection: "column",
-                    borderRadius: 2,
-                    alignItems: "center",
-                    width: "100%",
-                    padding: 5,
-                  }}
-                >
-                  {/*<Typography variant="h5" fontFamily={"abril_fatfaceregular"}>
+									<Typography variant="subtitle1" gutterBottom={true}>
+										<Trans i18nKey={"home.tutoriel.link"}>
+											<Link
+												href={t("home.tutoriel.link_url")}
+												target="_blank"
+												rel="noreferrer"
+											/>
+											<Link
+												href={t("home.tutoriel.video_link_url")}
+												target="_blank"
+												rel="noreferrer"
+											/>
+										</Trans>
+									</Typography>
+								</Box>
+							</Box>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<Stack
+								spacing={4}
+								paddingY={5}
+								marginX={10}
+								alignItems={"center"}
+							>
+								<Paper
+									sx={{
+										p: "2px 4px",
+										display: "flex",
+										flexDirection: "column",
+										borderRadius: 2,
+										alignItems: "center",
+										width: "100%",
+										padding: 5,
+									}}
+								>
+									{/*<Typography variant="h5" fontFamily={"abril_fatfaceregular"}>
                     {t("home.teachers")}
                   </Typography>*/}
-                  <Box
-                    sx={{
-                      justifyContent: "center",
-                      display: "flex",
-                      flexDirection: "column",
-                      alignContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Box
-                      sx={{
-                        width: 120,
-                      }}
-                    >
-                      <TeacherIcon />
-                    </Box>
+									<Box
+										sx={{
+											justifyContent: "center",
+											display: "flex",
+											flexDirection: "column",
+											alignContent: "center",
+											alignItems: "center",
+										}}
+									>
+										<Box
+											sx={{
+												width: 120,
+											}}
+										>
+											<TeacherIcon />
+										</Box>
 
-                    <Button
-                      data-testid="create"
-                      variant="contained"
-                      color="primary"
-                      size="large"
-                      sx={{ textTransform: "uppercase" }}
-                      onClick={() => handleCreate()}
-                    >
-                      {t("home.newProject")}
-                    </Button>
-                  </Box>
-                </Paper>
-                <Paper
-                  sx={{
-                    p: "2px 4px",
-                    display: "flex",
-                    flexDirection: "column",
-                    borderRadius: 2,
-                    alignItems: "center",
-                    width: "100%",
-                    padding: 5,
-                  }}
-                >
-                  {/*<Typography variant="h5" fontFamily={"abril_fatfaceregular"}>
+										<Button
+											data-testid="create"
+											variant="contained"
+											color="primary"
+											size="large"
+											sx={{ textTransform: "uppercase" }}
+											onClick={() => handleCreate()}
+										>
+											{t("home.newProject")}
+										</Button>
+									</Box>
+								</Paper>
+								<Paper
+									sx={{
+										p: "2px 4px",
+										display: "flex",
+										flexDirection: "column",
+										borderRadius: 2,
+										alignItems: "center",
+										width: "100%",
+										padding: 5,
+									}}
+								>
+									{/*<Typography variant="h5" fontFamily={"abril_fatfaceregular"}>
                     {t("home.students")}
                   </Typography>*/}
-                  <Box
-                    sx={{
-                      justifyContent: "center",
-                      display: "flex",
-                      flexDirection: "column",
-                      alignContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Box
-                      sx={{
-                        width: 120,
-                      }}
-                    >
-                      <StudentsIcon />
-                    </Box>
+									<Box
+										sx={{
+											justifyContent: "center",
+											display: "flex",
+											flexDirection: "column",
+											alignContent: "center",
+											alignItems: "center",
+										}}
+									>
+										<Box
+											sx={{
+												width: 120,
+											}}
+										>
+											<StudentsIcon />
+										</Box>
 
-                    <Button
-                      variant="contained"
-                      size="large"
-                      color="primary"
-                      onClick={() => handleJoin()}
-                      sx={{ textTransform: "uppercase" }}
-                    >
-                      {t("home.joinProject")}
-                    </Button>
-                  </Box>
-                </Paper>
-              </Stack>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
+										<Button
+											variant="contained"
+											size="large"
+											color="primary"
+											onClick={() => handleJoin()}
+											sx={{ textTransform: "uppercase" }}
+										>
+											{t("home.joinProject")}
+										</Button>
+									</Box>
+								</Paper>
+							</Stack>
+						</Grid>
+					</Grid>
+				</Container>
+			</Box>
 
-      <Suspense
-        fallback={
-          <Box
-            mx={2}
-            my={10}
-            display={"flex"}
-            alignContent={"center"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Box>
-              <CircularProgress />
-            </Box>
-          </Box>
-        }
-      >
-        <QueryErrorResetBoundary>
-          {({ reset }) => (
-            <ErrorBoundary
-              onReset={reset}
-              fallbackRender={({ resetErrorBoundary }) => (
-                <Fade in={true} appear={true}>
-                  <Typography
-                    variant="h6"
-                    align="center"
-                    gutterBottom={true}
-                    sx={{
-                      pt: 4,
-                      pb: 1,
-                    }}
-                  >
-                    <Trans i18nKey="ERR_UNKOWN" />
-                    <Button onClick={() => resetErrorBoundary()}>
-                      <Trans i18nKey={"home.projects.retry"}>Réessayer</Trans>
-                    </Button>
-                  </Typography>
-                </Fade>
-              )}
-            >
-              <ProjectGrid />
-            </ErrorBoundary>
-          )}
-        </QueryErrorResetBoundary>
-      </Suspense>
-    </Box>
-  );
+			<Suspense
+				fallback={
+					<Box
+						mx={2}
+						my={10}
+						display={"flex"}
+						alignContent={"center"}
+						justifyContent={"center"}
+						alignItems={"center"}
+					>
+						<Box>
+							<CircularProgress />
+						</Box>
+					</Box>
+				}
+			>
+				<QueryErrorResetBoundary>
+					{({ reset }) => (
+						<ErrorBoundary
+							onReset={reset}
+							fallbackRender={({ resetErrorBoundary }) => (
+								<Fade in={true} appear={true}>
+									<Typography
+										variant="h6"
+										align="center"
+										gutterBottom={true}
+										sx={{
+											pt: 4,
+											pb: 1,
+										}}
+									>
+										<Trans i18nKey="ERR_UNKOWN" />
+										<Button onClick={() => resetErrorBoundary()}>
+											<Trans i18nKey={"home.projects.retry"}>Réessayer</Trans>
+										</Button>
+									</Typography>
+								</Fade>
+							)}
+						>
+							<ProjectGrid />
+						</ErrorBoundary>
+					)}
+				</QueryErrorResetBoundary>
+			</Suspense>
+		</Box>
+	);
 };
